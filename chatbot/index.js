d2950d8823764983ed94392f5dce471734c498da
Terminator5931 <rajsssingh485@gmail.com>
2024-11-29 21:18:26 +0530

    chatbot

---
+++ chatbot/index.js
@@ -0,0 +1,10 @@
+console.log(
+  "i am the ghost of uchiha"
+    .split(" ")
+    .map((word) => word.slice(0,1).toUpperCase() + word.slice(1))
+    .join(" ")
+);
+// let word = "hianime";
+// let a = word.slice(0, 1);
+// let b = word.slice(1);
+// console.log(a.toUpperCase() + b);
