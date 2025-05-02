b7bdeade05744b81b710d0dac321aa69c952e71a
Terminator5931 <rajsssingh485@gmail.com>
2025-05-02 11:05:20 +0530

    changes

--- TODO_vite/src/main.js
+++
@@ -1,7 +0,0 @@
-import "./css/style.css";
-import "./css/content.css";
-import "./css/leftBar.css";
-import mainPageCompeonents from "./home";
-import "./jsonRef";
-import "./eventLis";
-mainPageCompeonents.render();
